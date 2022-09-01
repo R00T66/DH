cec368df9aac5c44069a5ae0594023ecaadd0404
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-09-01 06:57:02 +0100

    Update Main.lua

--- CrashV2/Main.lua
+++ CrashV2/Main.lua
@@ -1,5 +1,5 @@
 _G.Proximity = 175 -- Max Distance they can be from you
 
 loadstring(
-  game:HttpGet("https://raw.githubusercontent.com/R00T66/DaHood/main/CrashV2/Data/Script.XML"
+  game:HttpGet("https://raw.githubusercontent.com/R00T66/DaHood/main/CrashV2/Data/Script.XML")
 )()
