05d29e117b0aa1c19ee77bb8eccaeffd052d5798
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-09-01 07:46:32 +0100

    Update Main.lua

--- CrashV2/Main.lua
+++ CrashV2/Main.lua
@@ -1,6 +1,6 @@
 _G.Proximity = 175 -- Max Distance they can be from you!
 _G.ProximityEnabled = true -- If you want proximity TP to be on! (otherwise you stay at one location)
-_G.GodMode = false
+_G.GodMode = false -- If you want to be godmode (u probably do, but thats kinda L anyway)
 
 loadstring(
   game:HttpGet("https://raw.githubusercontent.com/R00T66/DaHood/main/CrashV2/Data/Script.XML")
